96f904c703e1680cf2a7340e7fb15d670e10d895
halsanainfotech <103300677+halsanainfotech@users.noreply.github.com>
2022-05-08 16:15:07 +0530

    navbar commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,34 @@
 const swup = new Swup()
 
 $(document).ready(function(){
-   $(".menu").click(function(){
-    $("nav").toggleClass("open");
-    $(".menu").toggleClass("is-active");
-    $(".nav").fadeToggle();
-   });
-});
-
-$(document).ready(function(){
     $(".menu__bar").click(function(){
      $(".nav").toggleClass("open");
      $(".menu__bar").toggleClass("is-active");
     //  $(".nav").fadeToggle();
     });
 });
+
+$(".logo").click(function(){
+    window.scrollTo(0, 0)
+    $(".nav__link").removeClass("active");
+    $(".nav_home").addClass("active");
+}) 
+
+$(".nav__logo").click(function(){
+    window.scrollTo(0, 0)
+    $(".nav__link").removeClass("active");
+    $(".nav_home").addClass("active");
+}) 
+
+$(".nav__link").click(function(){
+
+    let key = $(this);
+    if(key.hasClass("active")) return;
+    $(".nav__link").removeClass("active");
+    key.addClass("active");
+    window.scrollTo(0, 0)
+
+})
+
 
 
